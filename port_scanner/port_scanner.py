e4176917c7ea4b6d1b93df98de9879cfc16c0d14
Kafkacodes <rohitkyadav3108@gmail.com>
2026-06-19 19:11:28 +0530

    feat: Common port scanner

---
+++ port_scanner/port_scanner.py
@@ -0,0 +1,30 @@
+domain = input("Enter a domain name or IP address: ").strip()
+import socket
+
+common_ports = common_ports = [
+    21, 22, 23, 25, 53,
+    80, 110, 135, 139, 143,
+    443, 445,
+    993, 995,
+    1433, 3306,
+    3389, 5432,
+    5900, 6379,
+    8080, 8443,
+    9000, 9200,
+    27017
+]
+
+print(f"\nScanning common ports for {domain}...\n")
+
+for port in common_ports:
+    s = socket.socket()
+    s.settimeout(0.05)
+    
+    try:
+        name = socket.getservbyport(port)
+    except OSError:
+        name = "Unknown"
+    ip = socket.gethostbyname(domain)
+    if s.connect_ex((ip, port)) == 0:
+        print(f"[OPEN] Port {port} ({name})")
+    s.close()   
